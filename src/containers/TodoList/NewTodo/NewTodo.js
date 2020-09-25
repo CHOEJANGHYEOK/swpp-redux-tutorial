18717bb181fd6941f484bd17728cd595f65b73e2
CHOEJANGHYEOK <jh30056@gmail.com>
2020-09-25 19:13:42 +0900

    done

--- src/containers/TodoList/NewTodo/NewTodo.js
+++ src/containers/TodoList/NewTodo/NewTodo.js
@@ -1,48 +1,47 @@
 import React, { Component } from 'react';
 
-import { Redirect } from 'react-router-dom';
 
 import './NewTodo.css';
 
+import { connect } from 'react-redux';
+import * as actionCreators from '../../../store/actions/index';
+
 class NewTodo extends Component {
-  state = {
-    title: '',
-    content: '',
-    submitted: false
-  }
-
-  postTodoHandler = () => {
-    const data =
-      { title: this.state.title, content: this.state.content }
-    alert('submitted' + data.title);
-    // this.props.history.push('/todos');
-    this.props.history.goBack();
-    this.setState({ submitted: true });
-  }
-
-  render() {
-    let redirect = null;
-    if (this.state.submitted) {
-      redirect = <Redirect to='/todos' />
+    state = {
+        title: '',
+        content: '',
     }
-    return (
-      <div className="NewTodo">
-        <h1>Add a New Todo!</h1>
-        <label>Title</label>
-        <input
-          type="text"
-          value={this.state.title}
-          onChange={(event) => this.setState({ title: event.target.value })}
-        ></input>
-        <label>Content</label>
-        <textarea rows="4" type="text" value={this.state.content}
-          onChange={(event) => this.setState({ content: event.target.content })}
-        >
+
+    postTodoHandler = () => {
+        this.props.onStoreTodo(this.state.title, this.state.content);
+    }
+
+    render() {
+        return (
+            <div className="NewTodo">
+                <h1>Add a New Todo!</h1>
+                <label>Title</label>
+                <input
+                    type="text"
+                    value={this.state.title}
+                    onChange={(event) => this.setState({ title: event.target.value })}
+                ></input>
+                <label>Content</label>
+                <textarea rows="4" type="text" value={this.state.content}
+                          onChange={(event) => this.setState({ content: event.target.value })}
+                >
         </textarea>
-        <button onClick={() => this.postTodoHandler()}>Submit</button>
-      </div>
-    );
-  }
+                <button onClick={() => this.postTodoHandler()}>Submit</button>
+            </div>
+        );
+    }
 }
 
-export default NewTodo;
+const mapDispatchToProps = dispatch => {
+    return {
+        onStoreTodo: (title, content) =>
+            dispatch(actionCreators.postTodo({ title: title, content: content })),
+    }
+};
+
+export default connect(null, mapDispatchToProps)(NewTodo);
